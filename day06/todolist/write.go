907b2df99881bd347d8279171752930adea8f5ec
czhe <c18871384325@163.com>
2021-04-21 20:30:05 +0800

    demo

--- day06/todolist/write.go
+++ day06/todolist/write.go
@@ -20,13 +20,13 @@ func timestr(t *time.Time) string {
 	return t.Format(TimeLayte)
 }
 
-func (task *Task) WriteTo(file *os.File) error {
+func (Tasks *Task) WriteTo(file *os.File) error {
 	_, err := fmt.Fprintf(file, "%d,%s,%s,%s,%s\n",
-		task.id,
-		task.name,
-		task.user,
-		timestr(task.starttime),
-		timestr(task.endtime),
+		Tasks.id,
+		Tasks.name,
+		Tasks.user,
+		timestr(Tasks.starttime),
+		timestr(Tasks.endtime),
 	)
 	return err
 }
@@ -38,26 +38,31 @@ func main() {
 	end := time.Now().Add(10 * time.Hour)
 	endtime := &end
 
-	task := &[]Task{{
-		id:        1,
-		user:      "qing",
-		name:      "整理笔记1",
-		starttime: starttime,
-		endtime:   endtime,      //time.Now().Add(24 * time.Hour),
-	},
-	{
-		id: 2,
-		user: "cheng",
-		name: "整理笔记2",
-		starttime: starttime,
-		endtime: endtime,
-	},
-}
+	Tasks := &[]Task{}
+	Tasks = &[]Task{
+		{
+			id:        1,
+			user:      "qing",
+			name:      "整理笔记1",
+			starttime: starttime,
+			endtime:   endtime, //time.Now().Add(24 * time.Hour),
+		},
+		{
+			id:        2,
+			user:      "cheng",
+			name:      "整理笔记2",
+			starttime: starttime,
+			endtime:   endtime,
+		},
+	}
 
 	files, err := os.Create("task.log")
 	if err != nil {
 		return
 	}
 	defer files.Close()
-	task.WriteTo(files)
+	for _, task := range *Tasks {
+		task.WriteTo(files)
+	}
+
 }
